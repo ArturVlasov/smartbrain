76101a8321ff6086516202315246361b53598db3
Artur Vlasov <arturvlasov@yandex.ru>
2019-10-21 11:33:35 +0300

    Updates

---
+++ precache-manifest.2676d5439c714951291b4734a04caad1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fa9f563bf44d77069b7d28f074eaf591",
+    "url": "/smartbrain/index.html"
+  },
+  {
+    "revision": "c340537c3b723e7b5666",
+    "url": "/smartbrain/static/css/2.31dafeb2.chunk.css"
+  },
+  {
+    "revision": "336f59b50c9e56e840c8",
+    "url": "/smartbrain/static/css/main.4f2dbf71.chunk.css"
+  },
+  {
+    "revision": "c340537c3b723e7b5666",
+    "url": "/smartbrain/static/js/2.e6100c4d.chunk.js"
+  },
+  {
+    "revision": "336f59b50c9e56e840c8",
+    "url": "/smartbrain/static/js/main.56c1bda0.chunk.js"
+  },
+  {
+    "revision": "9ac85057d7b96f435469",
+    "url": "/smartbrain/static/js/runtime~main.e0aa727b.js"
+  }
+]);
